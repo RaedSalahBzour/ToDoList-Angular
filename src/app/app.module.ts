a08dec1b7b166f20f31e42c3683e7478e743ddf3
Raed Salah Bzour <raedbzour77@gmail.com>
2024-08-17 18:49:35 +0300

    installing required libraries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { TaskComponent } from './components/task/task.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TaskComponent,
+    AddTaskComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
