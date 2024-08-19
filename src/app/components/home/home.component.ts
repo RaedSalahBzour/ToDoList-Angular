075be4c67f90565ff36ed00f69701e6c196164af
Raed Salah Bzour <raedbzour77@gmail.com>
2024-08-19 23:22:28 +0300

    last commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { TasksService } from 'src/app/services/tasks.service';
+import { Task } from 'src/app/interfaces/task';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +9,14 @@ import { TasksService } from 'src/app/services/tasks.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  tasks: Array<Task> = [];
   constructor(public tasksService: TasksService, private title: Title) {
     title.setTitle('Task Manager');
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.tasks = this.tasksService.tasks;
+  }
   deleteTask(i: number) {
     this.tasksService.deleteTask(i);
   }
